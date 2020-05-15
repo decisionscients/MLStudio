3a64eea750c61e962314e05ef48511f8b8f543f1
daelmann <daelmann@gmail.com>
2020-05-15 18:09:37 -0400

    surface line plot animation

--- MLStudio/visual/animations/surface_line.py
+++ MLStudio/visual/animations/surface_line.py
@@ -19,12 +19,16 @@
 # Copyright (c) 2020 DecisionScients                                          #
 # =========================================================================== #
 """Animates gradient descent with surface and line plots."""
+import os
+
 import numpy as np
 import pandas as pd
 import plotly
 import plotly.express as px
 import plotly.graph_objects as go 
+import plotly.io as pio 
 import plotly.offline as py
+from plotly.subplots import make_subplots
 
 from mlstudio.utils.data_manager import todf
 # --------------------------------------------------------------------------  #
@@ -38,47 +42,50 @@ class SurfaceLine:
 
     def animate(self, model, directory=None, filename=None):
 
-        # Extract model history
-        history = model.blackbox_
+        # Extract model blackbox
+        blackbox = model.blackbox_
 
         # Extract the x=theta0, y=theta1 and cost history
-        weights = todf(history.epoch_log['theta'], stub='theta_')        
-        theta0 = weights['theta_0']
-        theta1 = weights['theta_1']
-        cost = history.epoch_log.get('train_costs')
+        weights = todf(blackbox.epoch_log['theta'], stub='theta_')        
+        theta0 = np.array(weights['theta_0'])
+        theta1 = np.array(weights['theta_1'])
+        cost = blackbox.epoch_log.get('train_cost')
 
         # Establish boundaries of plot
         theta0_min = min(theta0)
         theta1_min = min(theta1)
-        theta0_max = max(theta0) + abs(max(theta0)-min(theta0))
-        theta1_max = max(theta1) + abs(max(theta1)-min(theta1))
+        theta0_max = max(theta0) 
+        theta1_max = max(theta1) 
         theta0_mesh = np.linspace(theta0_min, theta0_max, 50)
         theta1_mesh = np.linspace(theta1_min, theta1_max, 50)
         theta0_mesh, theta1_mesh = np.meshgrid(theta0_mesh, theta1_mesh)
 
         # Create z axis grid based upon X,y and the grid of thetas
-        Js = np.array([self._cost_mesh(history.X, history.y, THETA)
+        Js = np.array([self._cost_mesh(model.X_train_, model.y_train_, THETA)
                     for THETA in zip(np.ravel(theta0_mesh), np.ravel(theta1_mesh))])
         Js = Js.reshape(theta0_mesh.shape)  
 
         # Initialize figure with 2 subplots
-        fig = make_subplots(rows=1, cols=2, subplot_titles=("Gradient Descent", "Linear Regression"))      
+        fig = make_subplots(rows=1, cols=2, subplot_titles=("Gradient Descent", "Linear Regression"),
+                            specs=[[{'type': "surface"}, {"type": "scatter"}]])      
 
         # Add surface and scatter plot
         fig.add_trace(
             go.Surface(x=theta0, y=theta1, z=Js, colorscale="YlGnBu", showscale=False),
-            row=1, col=1
-        )
+            row=1, col=1)
+
         fig.add_trace(
-            go.Scatter(x=model.X, y=model.y, row=1, col=2)
-        )
+            go.Scatter(x=model.X_train_, y=model.y_train_,
+                       name="ames",
+                       mode="markers",
+                       marker=dict(color="#1560bd")), row=1, col=2)
         # Create frames definition        
-        n_frames = len(theta0)
+        n_frames = len(theta0)        
         frames = [dict(
             name = k,
             data = [
-                go.Scatter3d(x=theta0[k], y=theta1[k], z=cost[k], mode='markers'),
-                go.Scatter(x=theta0[k], y=theta1[k])
+                go.Scatter3d(x=[theta0[k]], y=[theta1[k]], z=[cost[k]], mode='markers', marker=dict(color="red", size=16)),
+                go.Scatter(x=[theta0[k]], y=[theta1[k]], mode="lines")
             ],
             traces=[0,1]
             ) for k in range(n_frames)]
@@ -88,7 +95,7 @@ class SurfaceLine:
                             buttons=[dict(label="Play",
                                           method="animate",
                                           args=[[f'{k}' for k in range(n_frames)],
-                                            dict(frame=dict(duration=500, redraw=False),
+                                            dict(frame=dict(duration=100, redraw=False),
                                                  transition=dict(duration=0),
                                                  easing="linear",
                                                  fromcurrent=True,
@@ -97,10 +104,10 @@ class SurfaceLine:
                             pad=dict(r=10, t=85),
                             showactive=True, x=0.1, y=0, xanchor="right", yanchor="top")]
 
-        slider = [{"yanchor": "top",
+        sliders = [{"yanchor": "top",
                    "xanchor": "left",
                    "currentvalue": {"font": {"size": 16}, "prefix": "Iteration: ", "visible":True, "xanchor": "right"},
-                   'transition': {'duration': 500.0, 'easing': 'linear'},
+                   'transition': {'duration': 100.0, 'easing': 'linear'},
                    'pad': {'b': 10, 't': 50}, 
                    'len': 0.9, 'x': 0.1, 'y': 0, 
                    'steps': [{'args': [[k], {'frame': {'duration': 500.0, 'easing': 'linear', 'redraw': False},
@@ -109,8 +116,17 @@ class SurfaceLine:
                     ]}]
 
         fig.update(frames=frames)
-        fig.update_layout(updatemenus=updatemenus, sliders=sliders)
-        filepath = os.path.join(directory, filename)
-        fig.write_html(filepath, include_mathjax='cdn')
+        fig.update_layout(
+            xaxis=dict(range=[theta0_min, theta0_max], autorange=False, zeroline=False),
+            yaxis=dict(range=[theta1_min, theta1_max], autorange=False, zeroline=False),
+            title_text="Linear Regression by Gradient Descent", hovermode="closest",
+            title=dict(xanchor='center', yanchor='top', x=0.5, y=0.9),        
+            font=dict(family="Open Sans"),    
+            updatemenus=updatemenus, 
+            sliders=sliders, 
+            template='plotly_white')
+        if directory and filename:
+            filepath = os.path.join(directory, filename)
+            fig.write_html(filepath, include_mathjax='cdn')
         pio.renderers.default = "browser"
         fig.show()
